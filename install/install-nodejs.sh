945c5522471adc4c3e7ad78ee16f9ca954b517cb
niaka3dayo <65211260+niaka3dayo@users.noreply.github.com>
2024-04-15 09:24:36 +0900

    update: Node.jsのインストール方法を最新化
    最新LTS版の自動取得に変更し、手動でのバージョン指定を削除。

--- install/install-nodejs.sh
+++ install/install-nodejs.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# バージョン番号を指定 (e.g.: 20.9.0)
-NODE_VERSION="20.12.2"
-
-# nvmをインストール
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# nvmの最新バージョンを取得してインストール
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
 # nvm環境をロード
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# 指定されたバージョンのNode.jsをインストール
-nvm install $NODE_VERSION
+# Node.jsの最新LTS版を取得してインストール
+nvm install --lts
 
-# 指定されたバージョンのNode.jsを使用するように設定
-nvm use $NODE_VERSION
+# 最新LTS版のNode.jsを使用するように設定
+nvm use --lts
 
 # npmの最新バージョンをインストール
 npm install -g npm@latest
